fa739afa868f9181c3495e8ba7eaff000ceb614f
yutaro hirabayashi <hirataro.yu@gmail.com>
2025-10-02 09:31:59 +0900

    add: 動画リポジトリにページネーション機能を追加し、動画一覧取得メソッドを実装

--- src/modules/videos/video.repository.ts
+++ src/modules/videos/video.repository.ts
@@ -2,16 +2,34 @@ import api from "../../lib/api";
 import { Video } from "./video.entity";
 
 type CreateParams = {
-  video: File;
-  thumbnail: File;
-  title: string;
-  description: string;
-  isPublic: boolean;
-}
+	video: File;
+	thumbnail: File;
+	title: string;
+	description: string;
+	isPublic: boolean;
+};
+
+type Pagination = {
+	page: number;
+	limit: number;
+	totalPages: number;
+};
 
 export const videoRepository = {
-  async create(params: CreateParams):Promise<Video> {
-    const result = await api.postForm('/videos', params); // fileを含むデータをpostする時はpostFormを使う
-    return new Video(result.data);
-  }
-}
+	async create(params: CreateParams): Promise<Video> {
+		const result = await api.postForm("/videos", params); // fileを含むデータをpostする時はpostFormを使う
+		return new Video(result.data);
+	},
+
+	async find(
+		paginationParams: { page: number; limit: number } = { page: 1, limit: 9 }
+	): Promise<{ videos: Video[]; pagination: Pagination }> {
+		const result = await api.get("/videos", {
+			params: { ...paginationParams },
+		});
+		return {
+			videos: result.data.videos.map((video: Video) => new Video(video)),
+			pagination: result.data.pagination,
+		};
+	},
+};
